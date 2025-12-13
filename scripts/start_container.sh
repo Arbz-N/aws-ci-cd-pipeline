565e09dcf4e0db8221877011f9198252d3a93df4
Arbz-N <arbaznaeem555@gmail.com>
2025-12-13 13:49:22 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 echo "image is pulling"
 docker pull arbaznaeem/simple-python-flask-app
 
-docker run -d -p 5000:5000 arbaznaeem/simple-python-flask-app
+docker run -d -p 5000:5000 arbaznaeem/simple-python-flask-app /bin/bash

0362d38e569797866abbc83c63cc77586b92f357
Arbz-N <arbaznaeem555@gmail.com>
2025-12-13 13:37:14 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker pull docker pull arbaznaeem/simple-python-flask-app
+docker pull arbaznaeem/simple-python-flask-app
 
 docker run -d -p 5000:5000 arbaznaeem/simple-python-flask-app

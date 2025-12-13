608b046a20e7224b74eb833072195dfadef55c5c
Arbz-N <arbaznaeem555@gmail.com>
2025-12-13 14:13:04 +0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+APP_NAME=flask-app
+
+echo "Stopping old container if exists..."
+docker stop $APP_NAME || true
+docker rm $APP_NAME || true
